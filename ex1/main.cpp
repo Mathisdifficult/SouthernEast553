285c944f4e41423a56d4eb3e5a39399516bff6c4
杨联政 <702429162@qq.com>
2020-02-27 10:37:13 +0800

    Initial Commit

---
+++ ex1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ex1
+//
+//  Created by ylz on 2020/2/27.
+//  Copyright © 2020 ylz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
